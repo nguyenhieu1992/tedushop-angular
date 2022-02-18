abc5c21339b66334fddace6ef17b35a2a3498667
nguyenhieu1992 <92287350+nguyenhieu1992@users.noreply.github.com>
2022-02-18 10:21:40 +0700

    Initialize bai27

---
+++ tedushop-hieunn84/TeduShop.Service/NameDuplicatedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TeduShop.Service
+{
+    [Serializable]
+    internal class NameDuplicatedException : Exception
+    {
+        public NameDuplicatedException()
+        {
+        }
+
+        public NameDuplicatedException(string message) : base(message)
+        {
+        }
+
+        public NameDuplicatedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NameDuplicatedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
