fa2480b4f0eaf6811da30593680106a392c7b4b5
nguyenhieu1992 <92287350+nguyenhieu1992@users.noreply.github.com>
2022-02-18 10:21:46 +0700

    Initialize bai31

---
+++ tedushop-hieunn84/TeduShop.Data/Repositories/ApplicationGroupRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationGroupRepository : IRepository<ApplicationGroup>
+    {
+        IEnumerable<ApplicationGroup> GetListGroupByUserId(string userId);
+        IEnumerable<ApplicationUser> GetListUserByGroupId(int groupId);
+    }
+
+    public class ApplicationGroupRepository : RepositoryBase<ApplicationGroup>, IApplicationGroupRepository
+    {
+        public ApplicationGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ApplicationGroup> GetListGroupByUserId(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ApplicationUser> GetListUserByGroupId(int groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tedushop-hieunn84/TeduShop.Data/Repositories/ApplicationRoleGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationRoleGroupRepository : IRepository<ApplicationRoleGroup>
+    {
+    }
+
+    public class ApplicationRoleGroupRepository : RepositoryBase<ApplicationRoleGroup>, IApplicationRoleGroupRepository
+    {
+        public ApplicationRoleGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
