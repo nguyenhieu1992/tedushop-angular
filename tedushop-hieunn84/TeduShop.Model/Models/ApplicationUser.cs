1c9fed89c2c4b80786420fbc93c310ccea70e5b3
nguyenhieu1992 <92287350+nguyenhieu1992@users.noreply.github.com>
2022-02-18 10:21:44 +0700

    Initialize bai30

---
+++ tedushop-hieunn84/TeduShop.Model/Models/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    // Phan sua day nay 2
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string FullName { set; get; }
+
+        //Sua dòng code 2 ngày 17/10 2
+        [MaxLength(256)]
+        public string Address { set; get; }
+
+        public DateTime? BirthDay { set; get; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        public virtual IEnumerable<Order> Orders { set; get; }
+
+       
+    }
+
+}
