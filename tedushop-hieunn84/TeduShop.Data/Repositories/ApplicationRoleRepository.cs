2e8889b90b269f64e084455429a6d13da39b5a23
nguyenhieu1992 <92287350+nguyenhieu1992@users.noreply.github.com>
2022-02-18 10:21:49 +0700

    Initialize bai33

---
+++ tedushop-hieunn84/TeduShop.Data/Repositories/ApplicationRoleRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationRoleRepository : IRepository<ApplicationRole>
+    {
+        IEnumerable<ApplicationRole> GetListRoleByGroupId(int groupId);
+    }
+
+    public class ApplicationRoleRepository : RepositoryBase<ApplicationRole>, IApplicationRoleRepository
+    {
+        public ApplicationRoleRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ApplicationRole> GetListRoleByGroupId(int groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
